aa5abe4c1a8d61c024eca2a4b1db6f11f7e48468
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2024-03-27 22:43:57 -0600

    move

---
+++ src/Manuela/AppRouting/Router.cs
@@ -0,0 +1,32 @@
+﻿// The namespace not matching the folder is intentional, see #ABOUT-XAML-NS for more info.
+// DO NOT MOVE THE NS.
+
+namespace Manuela;
+
+public class Router
+{
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+    public static BindableProperty LinkProperty = BindableProperty.CreateAttached(
+        "Link", typeof(string), typeof(Has), null, propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            var route = (string)newValue;
+
+            if (bindable is Button button)
+            {
+                button.Clicked += (_, _) => AppRouting.Routing.GoTo(route);
+
+                return;
+            }
+
+            if (bindable is not View view) return;
+
+            view.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => AppRouting.Routing.GoTo(route))
+            });
+        });
+#pragma warning restore CA2211 // Non-constant fields should not be visible
+
+    public static string GetLink(BindableObject view) => (string)view.GetValue(LinkProperty);
+    public static void SetLink(BindableObject view, string value) => view.SetValue(LinkProperty, value);
+}
---
+++ src/Manuela/Styling/Has.cs
@@ -0,0 +1,78 @@
+﻿// The namespace not matching the folder is intentional, see #ABOUT-XAML-NS for more info.
+// DO NOT MOVE THE NS.
+
+using Manuela.Styling.ConditionalStyles.Screen;
+
+namespace Manuela;
+
+// it seems that Xaml intellisense does not work if attached properties are inherited..
+// lets write it here...
+public class Has
+{
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+    public static BindableProperty StylesProperty = BindableProperty.CreateAttached(
+        "Styles", typeof(StylesCollection), typeof(Has), null, propertyChanged: OnStyleCollectionChanged);
+
+    public static BindableProperty ScreenBreakPointProperty = BindableProperty.CreateAttached(
+        "ScreenBreakPoint", typeof(Breakpoint), typeof(Has), Breakpoint.Xs);
+
+    public static BindableProperty IsHoverStateProperty = BindableProperty.CreateAttached(
+        "IsHoverState", typeof(bool), typeof(Has), false);
+
+    public static BindableProperty IsPressedStateProperty = BindableProperty.CreateAttached(
+        "IsPressedState", typeof(bool), typeof(Has), false);
+
+    public static BindableProperty IsCheckedStateProperty = BindableProperty.CreateAttached(
+        "IsCheckedState", typeof(bool), typeof(Has), false);
+
+    public static BindableProperty IsValidStateProperty = BindableProperty.CreateAttached(
+        "IsValidState", typeof(bool), typeof(Has), true);
+
+    public static BindableProperty TransitionsProperty = BindableProperty.CreateAttached(
+        "Transitions", typeof(TransitionsCollection), typeof(Has), null);
+#pragma warning restore CA2211 // Non-constant fields should not be visible
+
+    public static StylesCollection GetStyles(BindableObject view)
+    {
+        return (StylesCollection)view.GetValue(StylesProperty);
+    }
+
+    public static void SetStyles(BindableObject view, StylesCollection value)
+    {
+        view.SetValue(StylesProperty, value);
+    }
+
+    public static TransitionsCollection GetTransitions(BindableObject view)
+    {
+        return (TransitionsCollection)view.GetValue(TransitionsProperty);
+    }
+
+    public static void SetTransitions(BindableObject view, TransitionsCollection value)
+    {
+        view.SetValue(TransitionsProperty, value);
+    }
+
+    public static void OnStyleCollectionChanged(BindableObject bindable, object? oldValue, object? newValue)
+    {
+        if (bindable is not VisualElement ve) return;
+
+        if (oldValue is not null and StylesCollection oldStylesCollection)
+        {
+            oldStylesCollection.Dispose();
+            foreach (var oldStyle in oldStylesCollection) oldStyle.Dispose();
+        }
+
+        var styleCollection = (StylesCollection?)newValue ?? [];
+        styleCollection.Initialize(ve);
+
+        foreach (var style in styleCollection)
+        {
+            if (ve.IsLoaded) style.Initialize(ve);
+            else ve.Loaded += (_, _) => style.Initialize(ve);
+
+            // on data templates it seems that the loaded event is not fired...
+            // possible workaround is to use SizeChanged:
+            // ve.SizeChanged += (_, _) => style.Initialize(ve);
+        }
+    }
+}
