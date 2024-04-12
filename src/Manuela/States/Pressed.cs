29b7ff29c4203b9c6b26f2b0a677291ebd981a4a
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2024-04-12 13:45:42 -0600

    better use our own implementation of "pressed" state.. the maui implementation seems bugged.

--- src/Manuela/States/Pressed.cs
+++ src/Manuela/States/Pressed.cs
@@ -33,47 +33,17 @@ public class Pressed : ConditionalStyle
     {
         _element = visualElement;
 
-        // we could just use the GetViewTriggers???
-        // probably yes... but lets use the Maui things for now
-
-        if (visualElement is Button button)
-        {
-            _propertyChangedEventHandler = (sender, e) =>
-            {
-                if (e.PropertyName is null or not (nameof(Button.IsPressed)))
-                    return;
-
-                visualElement.SetValue(Has.IsPressedStateProperty, button.IsPressed);
-            };
-
-            button.PropertyChanged += _propertyChangedEventHandler;
-        }
-        else if (visualElement is ImageButton imageButton)
-        {
-            _propertyChangedEventHandler += (sender, e) =>
-            {
-                if (e.PropertyName is null or not (nameof(ImageButton.IsPressed)))
-                    return;
-
-                visualElement.SetValue(Has.IsPressedStateProperty, imageButton.IsPressed);
-            };
-
-            imageButton.PropertyChanged += _propertyChangedEventHandler;
-        }
-        else
-        {
 #if DEBUG
-            if (visualElement is not View view)
-                throw new Exception(
-                    $"{nameof(Pressed)} trigger is not supported in elements of type {visualElement.GetType()}. " +
-                    $"The type does not inherit from {nameof(View)}");
+        if (visualElement is not View view)
+            throw new Exception(
+                $"{nameof(Pressed)} trigger is not supported in elements of type {visualElement.GetType()}. " +
+                $"The type does not inherit from {nameof(View)}");
 #endif
 
-            _behavior = new Behaviors.Behavior(visualElement);
+        _behavior = new Behaviors.Behavior(visualElement);
 
-            _behavior.Down += () => visualElement.SetValue(Has.IsPressedStateProperty, true);
-            _behavior.Up += () => visualElement.SetValue(Has.IsPressedStateProperty, false);
-        }
+        _behavior.Down += () => visualElement.SetValue(Has.IsPressedStateProperty, true);
+        _behavior.Up += () => visualElement.SetValue(Has.IsPressedStateProperty, false);
 
         Condition.Triggers = [new(visualElement, [Has.IsPressedStateProperty.PropertyName])];
     }
