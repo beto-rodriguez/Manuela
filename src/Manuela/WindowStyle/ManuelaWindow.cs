556189eacfaa81217c4279edf62f596bb8d7429d
beto rodriguez <rodriguezo.alberto@gmail.com>
2024-04-19 14:38:51 -0600

    try this

--- src/Manuela/WindowStyle/ManuelaWindow.cs
+++ src/Manuela/WindowStyle/ManuelaWindow.cs
@@ -1,6 +1,13 @@
-﻿#if ANDROID
+﻿using Microsoft.Maui.Platform;
+
+#if ANDROID
 using AndroidX.Core.View;
-using Microsoft.Maui.Platform;
+#endif
+
+#if IOS
+using ObjCRuntime;
+using UIKit;
+using Foundation;
 #endif
 
 namespace Manuela.WindowStyle;
@@ -24,6 +31,8 @@ public class ManuelaWindow
         }
 #endif
     }
+
+    // based on Maui Community Toolkit approach.
 
     public static void SetWindowColors(Color topColor, Color bottomColor)
     {
@@ -58,6 +67,109 @@ public class ManuelaWindow
         wic.SetSystemBarsAppearance(
             APPEARANCE_LIGHT_NAVIGATION_BARS, APPEARANCE_LIGHT_NAVIGATION_BARS);
 #endif
+
+#if IOS
+        var uIColor = topColor.ToPlatform();
+
+        if (OperatingSystem.IsIOSVersionAtLeast(13))
+        {
+            var statusBarTag = new IntPtr(38482);
+
+            //var windows = UIApplication.SharedApplication.Windows;
+
+            var windows = Application.Current?.Windows
+                .Select(x => x.Handler.PlatformView)
+                .Cast<UIWindow>()
+                .ToArray()
+                ?? [];
+
+            foreach (var uIWindow in windows)
+            {
+                var uIView = uIWindow.ViewWithTag(statusBarTag);
+
+                // suppressed because it is not reachable? the if clause must prevent that?
+
+#pragma warning disable CA1416 // Validate platform compatibility
+                var cGRect = uIWindow.WindowScene?.StatusBarManager?.StatusBarFrame;
+#pragma warning restore CA1416 // Validate platform compatibility
+
+                if (!cGRect.HasValue) continue;
+                uIView ??= new UIView(cGRect.Value);
+
+                uIView.Tag = statusBarTag;
+                uIView.BackgroundColor = uIColor;
+                uIView.TintColor = uIColor;
+#pragma warning disable CA1422 // Validate platform compatibility
+                uIView.Frame = UIApplication.SharedApplication.StatusBarFrame;
+#pragma warning restore CA1422 // Validate platform compatibility
+                foreach (var item in uIWindow.Subviews.Where((UIView x) => x.Tag == statusBarTag).ToList())
+                {
+                    item.RemoveFromSuperview();
+                }
+
+                uIWindow.AddSubview(uIView);
+                UpdateStatusBarAppearance(uIWindow);
+            }
+        }
+        else
+        {
+            if (
+                UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView uIView2 &&
+                uIView2.RespondsToSelector(new Selector("setBackgroundColor:"))
+                )
+            {
+                uIView2.BackgroundColor = uIColor;
+            }
+
+            UpdateStatusBarAppearance();
+        }
+
+        var isLight = topColor.Red * .2126 + topColor.Green * .7152 + topColor.Blue * .0722 > 0.5;
+
+        var style = isLight ? UIStatusBarStyle.BlackTranslucent : UIStatusBarStyle.DarkContent;
+        UIApplication.SharedApplication.SetStatusBarStyle(style, animated: false);
+
+        UpdateStatusBarAppearance();
+#endif
     }
+
+#if IOS
+    private static void UpdateStatusBarAppearance(UIWindow? window)
+    {
+        var uIViewController = window?.RootViewController
+            ?? WindowStateManager.Default.GetCurrentUIViewController()
+            ?? throw new InvalidOperationException("RootViewController cannot be null");
+
+        while (uIViewController.PresentedViewController != null)
+        {
+            uIViewController = uIViewController.PresentedViewController;
+        }
+
+        uIViewController.SetNeedsStatusBarAppearanceUpdate();
+    }
+
+    private static void UpdateStatusBarAppearance()
+    {
+        if (OperatingSystem.IsIOSVersionAtLeast(13))
+        {
+            //var windows = UIApplication.SharedApplication.Windows;
+
+            var windows = Application.Current?.Windows
+                .Select(x => x.Handler.PlatformView)
+                .Cast<UIWindow>()
+                .ToArray()
+                ?? [];
+
+            for (var i = 0; i < windows.Length; i++)
+            {
+                UpdateStatusBarAppearance(windows[i]);
+            }
+        }
+        else
+        {
+            UpdateStatusBarAppearance(UIApplication.SharedApplication.KeyWindow);
+        }
+    }
+#endif
 
 }
