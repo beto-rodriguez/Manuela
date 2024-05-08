e01a4a9a478e8572e255e86dcc96b07bb82daf73
beto rodriguez <rodriguezo.alberto@gmail.com>
2024-05-08 17:35:57 -0600

    pack

--- pack/pack.ps1
+++ pack/pack.ps1
@@ -2,21 +2,28 @@ param([string]$configuration = "Release", [string]$nupkgOutputPath = "./nupkg")
 
 $msbuild = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
 
-$project = "./src/Manuela/Manuela.csproj";
-
-& $msbuild $project -t:pack /p:configuration=$configuration /restore
-        
-$folder = $project.SubString(0, $project.LastIndexOf("/"))
-
 if (Test-Path $nupkgOutputPath) {
         Get-ChildItem $nupkgOutputPath -Include *.* -File -Recurse | ForEach-Object { $_.Delete() }
 }
 else {
         New-Item $nupkgOutputPath -ItemType "directory"
-}    
+}
 
-$found = Get-ChildItem $folder -Filter "*.nupkg" -Recurse -Force
-Copy-Item $found.FullName $nupkgOutputPath
+$projects = @("./src/Manuela/Manuela.csproj", "./src/Manuela.Generation/Manuela.Generation.csproj")
 
-$found = Get-ChildItem $folder -Filter "*.snupkg" -Recurse -Force
-Copy-Item $found.FullName $nupkgOutputPath
+foreach ($project in $projects) {
+    
+        & $msbuild $project -t:pack /p:configuration=$configuration /restore
+        
+        $folder = $project.SubString(0, $project.LastIndexOf("/"))
+
+        $found = Get-ChildItem $folder -Filter "*.nupkg" -Recurse -Force
+        Copy-Item $found.FullName $nupkgOutputPath
+
+        $found = Get-ChildItem $folder -Filter "*.snupkg" -Recurse -Force
+        # because the source generator do not generrrates .snupkg
+        if (![string]::IsNullOrEmpty($found))
+        {                
+                Copy-Item $found.FullName $nupkgOutputPath
+        }
+}
