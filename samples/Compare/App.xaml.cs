92add70d31f03e4a4864fe785a72bd7b88eb24a7
beto rodriguez <rodriguezo.alberto@gmail.com>
2024-05-09 21:34:19 -0600

    compare proj

---
+++ samples/Compare/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Compare;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new MainPage();
+    }
+}
---
+++ samples/Compare/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Compare;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
