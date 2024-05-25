a566577c40607b7bdede1d7533abfbdf047eab6d
beto rodriguez <rodriguezo.alberto@gmail.com>
2024-05-24 21:16:54 -0600

    also move this to forms assembly

---
+++ src/Manuela.Forms/Command.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace Manuela.Forms;
+
+public class Command(Action<object?> predicate) : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        predicate(parameter);
+    }
+}
---
+++ src/Manuela.Forms/IInputControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace Manuela.Forms;
+
+public interface IInputControl
+{
+    PropertyInput For { get; set; }
+    ICommand ValueChangedCommand { get; set; }
+    string ValidationMessage { get; set; }
+    void SetValue(object? value);
+    void SetPlaceholder(string placeholder);
+    void Dispatch(Action action);
+}
